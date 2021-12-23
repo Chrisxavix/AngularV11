40e00cbc05ab78d6ca8eafae7938d8d1812d82a3
Chrisxavix <chrischillogallid64@outlook.com>
2021-12-22 22:58:10 -0500

    chore: add input decorate

--- 01-bases/src/app/dbz/personajes/personajes.component.ts
+++ 01-bases/src/app/dbz/personajes/personajes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-personajes',
@@ -7,4 +8,5 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonajesComponent {
 
+  @Input('arrayPersonajes') personajes: Personaje[] = [];
 }
